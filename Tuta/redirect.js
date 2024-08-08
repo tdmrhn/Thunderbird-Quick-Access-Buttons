7dea33aa7214a26e85b376075f7b8a3c35e7369e
Taha Demirhan <dmrhn@gmx.com>
2024-08-08 12:56:16 +0300

    Logo changed

--- Tuta/redirect.js
+++ Tuta/redirect.js
@@ -1,4 +1,4 @@
-browser.spacesToolbar.addButton('Tutamail', {
+browser.spacesToolbar.addButton('Tuta', {
     title: "Tuta Mail",
     defaultIcons: "tuta.svg",
     url: "https://app.tuta.com/mail/"
