d4c5e0967536de23185157fb4b04f1ef5751f6d3
Taha Demirhan <dmrhn@gmx.com>
2024-09-09 13:21:53 +0300

    Added browser version check bypass

--- Microsoft Teams/redirect.js
+++ Microsoft Teams/redirect.js
@@ -3,3 +3,18 @@ browser.spacesToolbar.addButton('MicrosoftTeams', {
     defaultIcons: "teams.svg",
     url: "https://teams.microsoft.com/v2/"
 });
+
+
+browser.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    for (let header of details.requestHeaders) {
+      if (header.name.toLowerCase() === "user-agent") {
+        header.value = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/130.0";
+        break;
+      }
+    }
+    return { requestHeaders: details.requestHeaders };
+  },
+  { urls: ["https://teams.microsoft.com/*", "https://*.microsoft.com/*","https://teams.live.com/*", "https://*.live.com/*"] },
+  ["blocking", "requestHeaders"]
+);
