45d098492149bdb54c17136924aabd784a176918
Taha Demirhan <github@dmrhn.dev>
2026-02-03 09:12:22 +0300

    User Agent version updated

--- Microsoft Teams/redirect.js
+++ Microsoft Teams/redirect.js
@@ -9,7 +9,7 @@ browser.webRequest.onBeforeSendHeaders.addListener(
   function(details) {
     for (let header of details.requestHeaders) {
       if (header.name.toLowerCase() === "user-agent") {
-        header.value = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/130.0";
+        header.value = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0";
         break;
       }
     }
